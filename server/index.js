221df62c04b1fe479dc3b75f76135ab53ab77cab
Arvind Mishra <arvind.mishra1991@outlook.com>
2020-06-08 19:51:23 +0530

    added babel preset options

--- server/index.js
+++ server/index.js
@@ -3,7 +3,17 @@ require("ignore-styles");
 // eslint-disable-next-line no-undef
 require("@babel/register")({
   ignore: [/(node_module)/],
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    "@babel/preset-react",
+  ],
 });
 
 require("./server");
